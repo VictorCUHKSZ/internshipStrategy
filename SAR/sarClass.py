14583c6ab7b4a12c8ca8434244ddf438d56a6fc8
unknown <luotengy@qq.com>
2020-02-27 10:54:29 +0800

    SAR strategy

---
+++ SAR/sarClass.py
@@ -0,0 +1,48 @@
+import talib as ta
+import numpy as np
+import pandas as pd
+
+class sarSignal():
+
+    def __init__(self):
+        self.author = 'victor'
+    
+    def sar(self, am, paraDict):
+        accelerate = paraDict['acceleration']
+        rangePeriod = paraDict['rangePeriod']
+        
+        pastHigh = ta.MAX(am.high, rangePeriod) - 2*ta.STDDEV(am.high, rangePeriod)
+        pastLow = ta.MIN(am.low, rangePeriod) - 2*ta.STDDEV(am.low, rangePeriod)
+
+        sar = ta.SAR(pastHigh, pastLow, accelerate, 10*accelerate)
+        
+        return sar
+
+    def fliterVol(self, am, paraDict):
+        nDay = paraDict['nDay']
+        nFilter = paraDict['nFilter']
+        minPct = paraDict['minPct']
+        returns = am.close[-1]/am.close[0] - 1
+        
+        if am.volume<ta.SMA(am.volume, nDay)-nFilter*ta.STDDEV(am.volume, nDay) or returns<minPct:
+            filterCanTrade = -1
+        else:
+            filterCanTrade = 1
+        return filterCanTrade
+    
+    def bigVolWeight(self, am, paraDict):
+        bigVolPeriod = paraDict['bigVolPeriod']
+        bigVolThreshold = paraDict['bigVolThreshold']
+        bigMultiplier = paraDict['bigMultiplier']
+
+        rangeHL = ta.MAX(am.high, bigVolPeriod)-ta.MIN(am.low, bigVolPeriod)
+        bigVolRange = am.close[-1]*bigVolThreshold
+        bigVol = 1 if (rangeHL[-1] >= bigVolRange) else 0
+
+        lotMultiplier = 1
+        if bigVol:
+            lotMultiplier = bigMultiplier
+        return lotMultiplier
+
+
+        
